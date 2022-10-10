01cd6d84ad1994d7a0e0eb335b08b1aa99f970f7
三咲智子 <sxzz@sxzz.moe>
2022-10-11 01:33:29 +0800

    pnpm alias

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,18 +1,1 @@
-import FunctionalRef from 'vue-functional-ref/vite'
-
-export default defineNuxtConfig({
-  typescript: {
-    tsConfig: {
-      compilerOptions: {
-        baseUrl: '.',
-        paths: {
-          '@vue/reactivity': ['../node_modules/vue-functional-ref/types'],
-        },
-      },
-    },
-  },
-
-  vite: {
-    plugins: [FunctionalRef()],
-  },
-})
+export default defineNuxtConfig({})
